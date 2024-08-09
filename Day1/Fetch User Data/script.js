31bc290bf7cec0c33a5529f21b2ecdc8ceaf9713
Roshan Tapre <roshantapre666@gmail.com>
2024-08-09 17:55:59 +0530

    Work is done

---
+++ Day1/Fetch User Data/script.js
@@ -0,0 +1,27 @@
+document.getElementById("btn").addEventListener("click",fetch_user);
+
+
+async function fetch_user() {
+      const url ="https://reqres.in/api/users?page=2";
+      const response = await fetch(url) ;
+      const data = await response.json() ;
+      console.log(data);
+      display(data.data)
+}
+
+function display(users){
+    let container = document.querySelector(".user_container");
+    container.innerHTML = ''
+
+    users.forEach(element => {
+
+        const user = document.createElement("div");
+        user.className = "user";
+        user.innerHTML = `
+        <img src="${element.avatar}" alt="${element.first_name}">
+        <h2>${element.first_name} ${element.last_name}</h2>
+        <p>Email : ${element.email}</p>
+        `
+        container.appendChild(user)
+    });
+}
